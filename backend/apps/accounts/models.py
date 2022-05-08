9f051afe842ede6f6c6c88158384c62aee49d453
Macktireh <abdimack97@gmail.com>
2022-05-08 15:28:41 +0200

    Customize the Django User Model.

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from apps.accounts.managers import UserAccountManager
+
+
+class UserAccount(AbstractUser):
+    username = None
+    email = models.EmailField(
+        _('email address'), 
+        unique=True,
+        help_text=_("Required to authenticate")
+    )
+    is_email_verified = models.BooleanField(
+        _('email verified'), 
+        default=False,
+        help_text=_("Specifies whether the user should verify their email address.")
+    )
+    last_logout = models.DateTimeField(
+        _('last date logout'), 
+        blank=True, null=True,
+        help_text=_("last date logout user")
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserAccountManager()
+
+    def __str__(self):
+        return self.email
