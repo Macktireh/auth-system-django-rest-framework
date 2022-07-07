89630fcc64db494bccc13ad1d35f2d61ef55e76e
Macktireh <abdimack97@gmail.com>
2022-07-07 21:44:17 +0200

    testing user reset password seriamizer

--- backend/apps/account/tests/tests_serializers/test_user_reset_passwor_serializer.py
+++ backend/apps/account/tests/tests_serializers/test_user_reset_passwor_serializer.py
@@ -1,5 +1,8 @@
-from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 from apps.account import serializers
 
@@ -15,7 +18,41 @@ class UserResetPasswordSerializerTests(TestCase):
             'password': '12345',
         }
         self.serializer_data = {
-            'password': '12345',
-            'password2': '12345',
+            'password': '1234',
+            'password2': '1234',
         }
         self.user  = User.objects.create(**self.user_attributes)
+        self.uid = urlsafe_base64_encode(force_bytes(self.user.pk))
+        self.token = PasswordResetTokenGenerator().make_token(self.user)
+    
+    def test_user_reset_password_valid(self):
+        new_password_valid = self.serializer_data
+        serializer = serializers.UserResetPasswordSerializer(data=new_password_valid, context={'uid': self.uid, 'token': self.token})
+        self.assertTrue(serializer.is_valid())
+    
+    def test_user_reset_password_fields_is_required(self):
+        new_password_valid = self.serializer_data
+        serializer = serializers.UserResetPasswordSerializer(data={}, context={'uid': self.uid, 'token': self.token})
+        self.assertFalse(serializer.is_valid())
+    
+    def test_user_reset_password_invalid(self):
+        new_password_valid = self.serializer_data
+        new_password_valid['password2'] = '123'
+        serializer = serializers.UserResetPasswordSerializer(data=new_password_valid, context={'uid': self.uid, 'token': self.token})
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(str(serializer.errors['non_field_errors'][0]), 'Le mot de passe et le mot de passe de confirmation ne correspondent pas.')
+    
+    def test_user_reset_password_with_uid_invalid(self):
+        new_password_valid = self.serializer_data
+        self.uid = "ABC"
+        serializer = serializers.UserResetPasswordSerializer(data=new_password_valid, context={'uid': self.uid, 'token': self.token})
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(str(serializer.errors['non_field_errors'][0]), 'Votre demande de réinitialisation du mot de passe est expirer')
+
+    
+    def test_user_reset_password_with_token_invalid(self):
+        new_password_valid = self.serializer_data
+        self.token = "sopughkldhfreoyihyfuervnyegyytnghryei-14"
+        serializer = serializers.UserResetPasswordSerializer(data=new_password_valid, context={'uid': self.uid, 'token': self.token})
+        self.assertFalse(serializer.is_valid())
+        self.assertEqual(str(serializer.errors['non_field_errors'][0]), 'Votre demande de réinitialisation du mot de passe est expirer')
